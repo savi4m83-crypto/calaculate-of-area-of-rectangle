21c7a744dee8914c57b988a8eed569086042b27d
savi4m83-crypto <savi4m83@gmail.com>
2026-05-31 18:17:42 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# Write a function area(length, breadth) that returns the area of a rectangle.
+
+def area(length,breadth):
+    print(num1*num2)
+num1=int(input("Enter the num1 "))
+num2=int(input("Enter the num2 "))
+sum=num1*num2
+area(num1,num2)
